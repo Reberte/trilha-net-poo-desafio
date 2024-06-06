a9463f6768450b768093f4035ffab125d0391b42
Marcos Reberte <133255732+Reberte@users.noreply.github.com>
2024-06-06 15:40:16 -0300

    Projeto Finalizado

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,28 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+       
+        public Iphone(string numero) : base(numero)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Intalando o aplicativo:" + nomeApp);
+        }
+
+        // Implementando o metodo sobreescrevendo o metodo da classe Smartfone
+        public void Apresentar(string modelo, string imei, int memoria)
+        {
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            Console.WriteLine("--------------------");
+            Console.WriteLine($"Marca: Apple \n Número: {Numero} \n Modelo: {Modelo} \n Imei: {Imei} \n Memória: {Memoria}");
+            Console.WriteLine("--------------------");
+        }
     }
 }
